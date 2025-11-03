b30a130e9e4aba2535a26f0ae3964117175c7be7
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-11-03 19:08:20 +0900

    fix: pydantic_settings import 이슈 해결

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from functools import lru_cache
 
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
